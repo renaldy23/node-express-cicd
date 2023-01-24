5d505e518a241df7f62862241589700803c47eb1
renaldy23 <renaldyramadhan567@gmail.com>
2023-01-24 12:05:00 +0700

    add new router /about

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use('/', (req, res) => {
   })
 })
 
+app.get('/', (req, res) => {
+  res.json({
+    status: '200',
+    message: 'About pages',
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
