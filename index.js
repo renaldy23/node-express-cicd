5290c8d5382ec44d0547a94e1bc5017823b6c730
renaldy23 <renaldyramadhan567@gmail.com>
2023-01-24 13:18:24 +0700

    add route

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/pages', (req, res) => {
     message: 'All pages',
   })
 })
+app.get('/me', (req, res) => {
+  res.json({
+    status: '200',
+    message: 'Welcome my profile',
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
